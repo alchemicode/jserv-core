1744f3365ee4da18ef96a8eebe72d54b7971e22a
Kris TG <kketg03@gmail.com>
2023-11-30 07:12:28 -0500

    Updated response architecture

--- Response.go
+++ Response.go
@@ -1,51 +1,89 @@
 package core
 
-import "encoding/json"
+import (
+	"encoding/json"
 
-//Represents a server response after receiving and processing HTTP requests
+	toml "github.com/pelletier/go-toml/v2"
+	msg "github.com/vmihailenco/msgpack/v5"
+	yaml "gopkg.in/yaml.v3"
+)
+
+// Represents a server response after receiving and processing HTTP requests
 type Response struct {
 	// Whether the http request executed successfully ("ok" or "error")
-	Status string `json:"status"`
+	Status string `json:"status" msgpack:"status" csv:"status" toml:"status" yaml:"status"`
 	// Message explaining the status
-	Message string `json:"message"`
-	// JSON data containing anything pertinent to explaining the status
-	Data string `json:"data"`
+	Message string `json:"message" msgpack:"message" csv:"message" toml:"message" yaml:"message"`
+	// Data containing anything pertinent to the status
+	Data map[string]interface{} `json:"data,omitempty" msgpack:"data,omitempty" csv:"data,omitempty" toml:"data,omitempty" yaml:"data,omitempty,flow"`
 }
 
-// Constructs a response without the inclusion of JSON data
+// Constructs a response without the inclusion of data
 func (r *Response) WithoutData(status string, message string) {
 	r.Status = status
 	r.Message = message
-	r.Data = ""
+	r.Data = map[string]interface{}{}
 }
 
-// Constructs a response with the inclusion of JSON data
-func (r *Response) WithData(status string, message string, data string) {
+// Constructs a response with the inclusion of data
+func (r *Response) WithData(status string, message string, data map[string]interface{}) {
 	r.Status = status
 	r.Message = message
 	r.Data = data
 }
 
-//Converts the JSON string field Data to a map
-func (r Response) DataToMap() (map[string]interface{}, error) {
-	var data map[string]interface{}
-	if err := json.Unmarshal([]byte(r.Data), &data); err != nil {
-		return map[string]interface{}{}, err
-	}
-	return data, nil
+// Converts the Response object into JSON
+func (r Response) ToJson() string {
+	js, _ := json.Marshal(r)
+	return string(js)
 }
 
-//Converts the Response object into JSON
-func (r Response) ToJson() string {
-	data := make(map[string]interface{})
-	data["status"] = r.Status
-	data["message"] = r.Message
-	if r.Data != "" {
-		dat, err := r.DataToMap()
-		if err != nil {
-			data["data"] = dat
-		}
+// Converts the Mod into MsgPack
+func (r Response) ToMsgPack() string {
+	mp, _ := msg.Marshal(r)
+	return string(mp)
+}
+
+// Converts the Mod into TOML
+func (r Response) ToToml() string {
+	t, _ := toml.Marshal(r)
+	return string(t)
+}
+
+// Converts the Mod into YAML
+func (r Response) ToYaml() string {
+	y, _ := yaml.Marshal(r)
+	return string(y)
+}
+
+// JSON Constructor
+// Creates the Mod object from JSON
+func (r *Response) FromJson(s string) {
+	if err := json.Unmarshal([]byte(s), r); err != nil {
+		panic(err)
 	}
-	js, _ := json.Marshal(data)
-	return string(js)
+}
+
+// MsgPack Constructor
+// Creates the Mod object from MsgPack
+func (r *Response) FromMsgPack(s string) {
+	if err := msg.Unmarshal([]byte(s), r); err != nil {
+		panic(err)
+	}
+}
+
+// TOML Constructor
+// Creates the Mod object from TOML
+func (r *Response) FromToml(s string) {
+	if err := toml.Unmarshal([]byte(s), r); err != nil {
+		panic(err)
+	}
+}
+
+// YAML Constructor
+// Creates the Mod object from YAML
+func (r *Response) FromYaml(s string) {
+	if err := yaml.Unmarshal([]byte(s), r); err != nil {
+		panic(err)
+	}
 }
